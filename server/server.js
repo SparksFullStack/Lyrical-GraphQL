55945d2314821e4699551ba7455712da360d0fee
SparksFullStack <csparks@antioch.edu>
2019-04-02 16:42:08 -0700

    initial setup compelte

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = "mongodb://admin:adminpassword12@ds042888.mlab.com:42888/sfs-lyrical-graphql";
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
